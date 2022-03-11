fd5a790b4b20ac457c4994fa7a90f397e903b4d1
Harsha Prabhu <astroharsha@gmail.com>
2022-03-11 22:30:35 +0530

    Made functioning toggle button

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('hi');
+// nav js
 const nav = document.querySelector('.nav');
 console.dir(nav.classList)
 window.onscroll = function() {myfunc()};
@@ -13,4 +13,22 @@ function myfunc(){
     else{
         nav.classList.remove('color');
     }
+}
+
+//chooser js
+var cusb = document.querySelector('.bl14');
+var capb = document.querySelector('.bl24');
+var customer = document.querySelector('.customers');
+var captains = document.querySelector('.Captains');
+cusb.onclick = function(){
+    captains.classList.add('vanish');
+    customer.classList.remove('vanish');
+    cusb.classList.add('yelltog');
+    capb.classList.remove('yelltog');
+}
+capb.onclick = function(){
+    customer.classList.add('vanish');
+    captains.classList.remove('vanish');
+    capb.classList.add('yelltog');
+    cusb.classList.remove('yelltog');
 }
